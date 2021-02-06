a6d086f66f9e859286b896cc90bc78c7b955bd07
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-02-06 17:55:57 -0500

    I have added the index part of the website

--- Assets/JS/style.js
+++ Assets/JS/style.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const show = document.querySelector('.show-hide')
 const qaContainer = document.getElementById('qaJS')
 
@@ -30,3 +31,25 @@ function convertFun(){
        `
     }
 }
+=======
+// const show = document.querySelector('.show-hide')
+
+// show.addEventListener('click', convertFun)
+
+// function convertFun(){
+//     var hideContent= document.querySelector('.hide-content')
+//     if (hideContent.style.display === "none"){
+//       hideContent.style.display = 'block'
+//     } else {
+//       hideContent.style.display = "none"
+//       hideContent.innerHTML = 
+//       `<p>Verified experts have completed comprehensive training based on CollegeVine’s proprietary data and research in the field of college admissions.
+//       Their names might look familiar because many of them host our free livestreams.</p>
+//       <p>These experts are available for free on CollegeVine because we know that not everybody has access to a school counselor or private tutor when they need help.
+//          You can ask a question any time and guarantee an expert answer for 20 karma.</p>
+//     <p>Don't have karma?
+//       You can still submit a question without karma and get answers from your peers or a verified expert if they are online! 
+//       </p>  `
+//     }
+// }
+>>>>>>> bbcf804... I have added the index part of the website
